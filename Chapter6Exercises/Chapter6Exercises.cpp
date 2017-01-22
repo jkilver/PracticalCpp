d1488656b5c27eacc5108628377ab232870c84db
Jacob Kilver <robobookworm@gmail.com>
2017-01-22 08:44:00 -0500

    Chapter 6 Exercise 2

--- Chapter6Exercises/Chapter6Exercises.cpp
+++ Chapter6Exercises/Chapter6Exercises.cpp
@@ -6,44 +6,82 @@
 
 using namespace std;
 
+/// Used to determine whether letter grade should
+/// have a plus (+), minus (-), or nothing
+/// Input expected to be between 0 and 10
+char DetermineModifier(int lastDigit)
+{
+	char retVal = ' ';
+
+	if (lastDigit > 0 && lastDigit <= 3)
+	{
+		retVal = '-';
+	}
+	else if (lastDigit >= 8)
+	{
+		retVal = '+';
+	}
+
+	return retVal;
+}
+
 int main()
 {
 	int grade;
-	char letterGrade = ' ';
-	char modifier = ' ';
-
-	cout << "Input a positive whole number numeric grade: ";
-	cin >> grade;
-
-	if (grade < 0 || grade > 100)
-	{
-		cout << "\nNot a valid grade.";
-	}
-
+	char letterGrade;
+	char modifier;
 	
-	if (grade >= 0 && grade <= 60)
+	while (true)
 	{
-		// Failing grade, no modifier
-		letterGrade = 'F';
+		// Reset
+		letterGrade = ' ';
+		modifier = ' ';
+
+		// Get the grade
+		cout << "Input a positive whole number numeric grade, -1 to exit: ";
+		cin >> grade;
+
+		// Some simple input validation
+		if (grade == -1)
+		{
+			break;
+		}
+
+		if (grade < 0 || grade > 100)
+		{
+			cout << "\nNot a valid grade.";
+			continue;
+		}
+		
+		// Determine grade 
+		if (grade >= 0 && grade <= 60)
+		{
+			// Failing grade, no modifier
+			letterGrade = 'F';
+		}
+		else if (grade > 60 && grade <= 70)
+		{
+			letterGrade = 'D';
+			modifier = DetermineModifier(grade - 60);
+		}
+		else if (grade > 70 && grade <= 80)
+		{
+			letterGrade = 'C';
+			modifier = DetermineModifier(grade - 70);
+		}
+		else if (grade > 80 && grade <= 90)
+		{
+			letterGrade = 'B';
+			modifier = DetermineModifier(grade - 80);
+		}
+		else if (grade > 90)
+		{
+			letterGrade = 'A';
+			modifier = DetermineModifier(grade - 90);
+		}
+
+		cout << "Letter grade is: " << letterGrade << modifier << "\n\n";
 	}
-	else if (grade > 60 && grade <= 70)
-	{
-		letterGrade = 'D';
-	}
-	else if (grade > 70 && grade <= 80)
-	{
-		letterGrade = 'C';
-	}
-	else if (grade > 80 && grade <= 90)
-	{
-		letterGrade = 'B';
-	}
-	else if (grade > 90)
-	{
-		letterGrade = 'A';
-	}
-	
-	cout << "Letter grade is: " << letterGrade;
 
 	return 0;
 }
