337b1abbba30a0a991da2eef7918b789aabd0409
Jacob Kilver <robobookworm@gmail.com>
2017-01-22 08:28:19 -0500

    Ch 6 Exercise 1

--- Chapter6Exercises/Chapter6Exercises.cpp
+++ Chapter6Exercises/Chapter6Exercises.cpp
@@ -4,15 +4,46 @@
 #include "stdafx.h"
 #include <iostream>
 
+using namespace std;
 
 int main()
 {
 	int grade;
-	std::cout << "Input a positive whole number numeric grade: ";
-	std::cin >> grade;
+	char letterGrade = ' ';
+	char modifier = ' ';
 
-	std::cout << "\nHere is the grade you entered: " << grade << "\n";
+	cout << "Input a positive whole number numeric grade: ";
+	cin >> grade;
 
+	if (grade < 0 || grade > 100)
+	{
+		cout << "\nNot a valid grade.";
+	}
+
+	
+	if (grade >= 0 && grade <= 60)
+	{
+		// Failing grade, no modifier
+		letterGrade = 'F';
+	}
+	else if (grade > 60 && grade <= 70)
+	{
+		letterGrade = 'D';
+	}
+	else if (grade > 70 && grade <= 80)
+	{
+		letterGrade = 'C';
+	}
+	else if (grade > 80 && grade <= 90)
+	{
+		letterGrade = 'B';
+	}
+	else if (grade > 90)
+	{
+		letterGrade = 'A';
+	}
+	
+	cout << "Letter grade is: " << letterGrade;
 
 	return 0;
 }
