9ada9bf6413bca4ed29bf1c39d9605fed94f8422
Jacob Kilver <robobookworm@gmail.com>
2017-01-22 08:16:58 -0500

    Chapter 6 exercises

---
+++ Chapter6Exercises/Chapter6Exercises.cpp
@@ -0,0 +1,19 @@
+// Chapter6Exercise.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+
+int main()
+{
+	int grade;
+	std::cout << "Input a positive whole number numeric grade: ";
+	std::cin >> grade;
+
+	std::cout << "\nHere is the grade you entered: " << grade << "\n";
+
+
+	return 0;
+}
+
---
+++ Readme.md
@@ -0,0 +1,3 @@
+This project contains exercises from the book "Practical C++ Programming" by Steve Oualline.
+
+I will be using Visual Studio 2015 and Visual C++
